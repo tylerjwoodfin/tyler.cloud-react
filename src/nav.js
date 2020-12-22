1b8c9b20bb34a412593705bc9e4c2d2baba22567
Tyler Woodfin <tylerjwoodfin@gmail.com>
2020-12-21 22:47:58 -0600

    Continued nav progress

--- src/nav.js
+++ src/nav.js
@@ -11,14 +11,27 @@ const projects = {title: "Projects", items: [
 const nav = [
     {title: "Home", link: "/"},
     {title: "Github", link: "https://github.com/tylerjwoodfin"},
-    {title: "LinkedIn", link: "https://linkedin.com/in/tylerjwoodfin"}
+    {title: "LinkedIn", link: "https://linkedin.com/in/tylerjwoodfin"},
+    projects
 ];
 
 export class Nav extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.toggleProjects = this.toggleProjects.bind(this);
+    }
+
+    toggleProjects() {
+        alert("Toggle");
+    }
+
     render() {
-        const navItems = nav.map(function mapper(item) {
-            return item.items === undefined ? <li key={item.title}><a href={item.link} target="_blank">{item.title}</a></li> : <li key={item.title}>{item.title}<ul>{item.items.map(mapper)}</ul></li>;
+        const navItems = nav.map((item) => {
+            // return item.items === undefined ? <li key={item.title}><a href={item.link} target="_blank">{item.title}</a></li> : <li key={item.title}>{item.title}<ul>{item.items.map(mapper)}</ul></li>;
+            return item.items === undefined 
+            ? <a key={item.title} href={item.link} target="_blank" rel="noreferrer"><li>{item.title}</li></a>
+            : <li key={item.title} onClick={this.toggleProjects}>{item.title}<i className="fa fa-caret-down"></i></li>;
         });
 
         return (
